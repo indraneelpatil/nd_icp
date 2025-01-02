2cc66a81b15647b13d9e1003b04061718103674d
Indraneel Patil <Neil@Neils-MacBook-Pro.local>
2025-01-02 08:02:09 -0800

    Updated doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # nd-ICP
+//!
+//! `nd-icp` is a generic n dimensional implementation of the ICP algorithm based on SVD and the
+//! nalgebra linear algebra library
 pub mod icp;
 pub mod types;
 pub mod utils;
